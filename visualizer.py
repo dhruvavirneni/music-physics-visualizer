4a4a4d6b6403244a8011a7b8a459fad864eec67c
Dhruv Avirneni <dhruv.avirneni@gmail.com>
2025-09-30 22:42:36 -0500

    testing flask

--- visualizer.py
+++ visualizer.py
@@ -1,15 +1,28 @@
 import librosa
 import librosa.display
 import matplotlib.pyplot as plt
+from flask import Flask, render_template
 
 
 # load audio file
-file = r"C:\Users\avirn\Dhruvcoding\music-physics-visualizer\audio\crsound.wav"
-y, sr = librosa.load(file, sr=None)
 
-plt.figure(figsize=(10, 6))
-librosa.display.waveshow(y, sr=sr)
-plt.title("Waveform of Audio")
-plt.xlabel("Time (s)")
-plt.ylabel("Amplitude")
-plt.show()
+#file = r"C:\Users\avirn\Dhruvcoding\music-physics-visualizer\audio\crsound.wav"
+#y, sr = librosa.load(file, sr=None)
+
+#plt.figure(figsize=(10, 6))
+#librosa.display.waveshow(y, sr=sr)
+#plt.title("Waveform of Audio")
+#plt.xlabel("Time (s)")
+#plt.ylabel("Amplitude")
+#plt.show()
+
+app = Flask(__name__)
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/<name>')
+def user(name):
+    return render_template('second.html', name=name)
+
+app.run()
